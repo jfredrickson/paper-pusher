8d9fff5a2a50ba40f8e6a7c75bc883ad05cf404c
Jeff Fredrickson <jfredrickson@gmail.com>
2010-10-04 15:19:58 -0400

    created User model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  # Devise modules
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable,
+         :validatable
+  
+  # Setup accessible (or protected) attributes
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+end
