e8d5ee80a721300d37dbebc4a64c719f39b9d8ef
Jeff Fredrickson <jfredrickson@gmail.com>
2010-10-04 15:00:28 -0400

    Added devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,12 @@ PaperPusher::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  ### ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'localhost' }
+  # A dummy setup for development - no deliveries, but logged
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
 end
