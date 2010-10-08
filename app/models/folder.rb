1326706b35480737a3e39f91f4797c5fdc861469
Jeff Fredrickson <jfredrickson@gmail.com>
2010-10-08 19:48:04 -0400

    remove cabinet_id validation

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -2,7 +2,6 @@ class Folder < ActiveRecord::Base
   attr_accessible :name, :description
   
   validates :name, :presence => true
-  validates :cabinet_id, :presence => true
   
   belongs_to :cabinet
   
