625bde5bd9ac94e48eb7134676905a0a9b16376a
Jeff Fredrickson <jfredrickson@gmail.com>
2010-10-11 21:06:01 -0400

    add initial content types for future use

--- app/models/document.rb
+++ app/models/document.rb
@@ -9,4 +9,21 @@ class Document < ActiveRecord::Base
   default_scope order('name')
   
   accepts_nested_attributes_for :revisions, :allow_destroy => true
+  
+  CONTENT_TYPES = {
+    "application/pdf" => "PDF",
+    "application/msword" => "Microsoft Word",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document" => "Microsoft Word",
+    "application/vnd.ms-excel" => "Microsoft Excel",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" => "Microsoft Excel",
+    "application/vnd.ms-powerpoint" => "Microsoft Powerpoint",
+    "application/vnd.openxmlformats-officedocument.presentationml.presentation" => "Microsoft Powerpoint",
+    "text/plain" => "plain text",
+    "text/html" => "HTML",
+    "image/png" => "PNG image",
+    "image/jpeg" => "JPEG image",
+    "image/gif" => "GIF image",
+    "video/quicktime" => "QuickTime movie",
+    "application/zip" => "zip archive"
+  }
 end
