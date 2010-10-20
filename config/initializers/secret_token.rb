4068486bb6cfa6b67adee91ffa7a2b5c68a355b7
Jeff Fredrickson <jfredrickson@gmail.com>
2010-10-20 13:02:55 +0000

    remove secret_token.rb from source control

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-PaperPusher::Application.config.secret_token = 'ef79138403001e3523656e005ab7dc7fa43f0ed99721d81647ac3d80761a5914b19f900fdb73ab04246241b8324416cbf06c8914ca1fc904daaa5365e3a1b562'
